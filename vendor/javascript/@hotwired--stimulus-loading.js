2f8dd5cbe9d8845650446ddbcdb5c9ba49dd8d6e
t0j1 <69952064+t0j1@users.noreply.github.com>
2025-11-23 08:13:54 +0000

    Fix: @hotwired--stimulus-loading.js の内容を修正

--- vendor/javascript/@hotwired--stimulus-loading.js
+++ vendor/javascript/@hotwired--stimulus-loading.js
@@ -1,2 +1,23 @@
-<!doctype html>
-    <p>Not Found.</p>
+import { Application } from "@hotwired/stimulus"
+
+export function eagerLoadControllersFrom(path, application) {
+  const modules = import.meta.glob("/app/javascript/controllers/**/*_controller.js", { eager: true })
+  for (const [modulePath, module] of Object.entries(modules)) {
+    const name = modulePath
+      .match(/controllers\/(.+)_controller\.js$/)[1]
+      .replace(/\//g, "--")
+      .replace(/_/g, "-")
+    application.register(name, module.default)
+  }
+}
+
+export function lazyLoadControllersFrom(path, application) {
+  const modules = import.meta.glob("/app/javascript/controllers/**/*_controller.js")
+  for (const [modulePath, importModule] of Object.entries(modules)) {
+    const name = modulePath
+      .match(/controllers\/(.+)_controller\.js$/)[1]
+      .replace(/\//g, "--")
+      .replace(/_/g, "-")
+    application.lazyRegister(name, importModule)
+  }
+}
